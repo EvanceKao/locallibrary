5bf6d36875d7203e52a80fbb43c92374e7b17163
EvanceTW <content@evance.tw>
2020-08-05 02:28:45 +0800

    Finished Django Tutorial Part 6

--- catalog/urls.py
+++ catalog/urls.py
@@ -3,9 +3,10 @@ from . import views
 
 
 urlpatterns = [
-
-]
-
-urlpatterns = [
     path('', views.index, name='index'),
+    path('books/', views.BookListView.as_view(), name='books'),
+    path('book/<int:pk>', views.BookDetailView.as_view(), name='book-detail'),
+    # This method is used just like path() except that it allows you to specify a pattern using a Regular expression.
+    # For example, the previous path could have been written as shown below:
+    # re_path(r'^book/(?P<pk>\d+)$', views.BookDetailView.as_view(), name='book-detail'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from .models import Book, Author, BookInstance, Genre
+from django.views import generic
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
 
@@ -25,3 +27,48 @@ def index(request):
 
     # Render the HTML template index.html with the data in the context variable
     return render(request, 'index.html', context=context)
+
+class BookListView(generic.ListView):
+    model = Book
+    paginate_by = 3
+    # context_object_name = 'my_book_list'  # your own name for the list as a template variable
+    # queryset = Book.objects.filter(title__icontains='war')[:5]  # Get 5 books containing the title war
+    # template_name = 'books/my_arbitrary_template_name_list.html'  # Specify your own template name/location
+
+    # Overriding methods in class-based views
+    # 例如，我們可以覆寫get_queryset（）方法，來更改返回的記錄列表。
+    # 這比單獨設置queryset屬性更靈活，就像我們在前面的代碼片段中進行的那樣（儘管在這案例中沒有太大用處）：
+    # def get_queryset(self):
+    #     return Book.objects.filter(title__icontains='war')[:5] # Get 5 books containing the title war
+
+
+    # Overriding methods in class-based views
+    # 我們還可以重寫get_context_data() 以便將其他上下文變數傳遞給模組 (例如，默認情況下傳遞書籍列表).
+    # 下面的片段顯示瞭如何向上下文添加名為"some_data" 的變數（然後它將用作模組變數）
+    # def get_context_data(self, **kwargs):
+    #     # Call the base implementation first to get the context
+    #     context = super(BookListView, self).get_context_data(**kwargs)
+    #     # Create any data and add it to the context
+    #     context['some_data'] = 'This is just some data'
+    #     return context
+
+    # 執行此操作時，務必遵循上面使用的模式：
+    # 首先從我們的superclass中獲取現有內文。
+    # 然後添加新的內文信息。
+    # 然後返回新的（更新後）內文。
+
+
+class BookDetailView(generic.DetailView):
+    model = Book
+
+    # Just to give you some idea of how this works, the code fragment below demonstrates how you would implement the class-based view as a function, if you were not using the generic class-based detail view.
+    # def book_detail_view(request, primary_key):
+    #     try:
+    #         book = Book.objects.get(pk=primary_key)
+    #     except Book.DoesNotExist:
+    #         raise Http404('Book does not exist')
+    #
+    #     # from django.shortcuts import get_object_or_404
+    #     # book = get_object_or_404(Book, pk=primary_key)
+    #
+    #     return render(request, 'catalog/book_detail.html', context={'book': book})
