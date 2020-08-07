c47122969e86defbc5b6810d3e0635f91568b75d
EvanceTW <content@evance.tw>
2020-08-08 02:19:06 +0800

    Finished Django Tutorial Part 9

--- catalog/urls.py
+++ catalog/urls.py
@@ -13,4 +13,38 @@ urlpatterns = [
 
 urlpatterns += [
     path('mybooks/', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),
+    path(r'borrowed/', views.LoanedBooksAllListView.as_view(), name='all-borrowed'),  # Added for challenge
 ]
+
+#圖書館管理人員限定的 更新讀者書本到期日的功能
+#網址格式：/catalog/book/<bookinstance id>/renew/
+#renew_book_librarian是底線分隔，表示這是一個function-based view
+urlpatterns += [
+    path('book/<uuid:pk>/renew/', views.renew_book_librarian, name='renew-book-librarian'),
+]
+
+#圖書館管理人員限定的 更新讀者書本到期日的功能(改用modelform的方式實做)
+#網址格式：/catalog/book/<bookinstance id>/renew_bymodelform/
+#renew_book_librarian是底線分隔，表示這是一個function-based view
+urlpatterns += [
+    path('book/<uuid:pk>/renew_bymodelform/', views.renew_book_librarian_modelform, name='renew-book-librarian-modelform'),
+]
+
+
+
+#modelform實做範例
+#Author資料利用modelform快速建立create, update, delete功能
+#modelform的快速建立功能相當類似asp.net mvc的skeleton
+urlpatterns += [
+    path('author/create/', views.AuthorCreate.as_view(), name='author_create'),
+    path('author/<int:pk>/update/', views.AuthorUpdate.as_view(), name='author_update'),
+    path('author/<int:pk>/delete/', views.AuthorDelete.as_view(), name='author_delete'),
+]
+
+#加入Authors資料表的list清單網頁的url mapping
+urlpatterns += [
+    path('authors/', views.AuthorListView.as_view(), name='authors'),
+]
+
+
+
--- catalog/views.py
+++ catalog/views.py
@@ -1,9 +1,13 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from .models import Book, Author, BookInstance, Genre
 from django.views import generic
 from django.shortcuts import get_object_or_404
-from django.contrib.auth.decorators import login_required   # Part 8
-from django.contrib.auth.mixins import LoginRequiredMixin   # Part 8
+from django.contrib.auth.decorators import login_required       # Part 8
+from django.contrib.auth.mixins import LoginRequiredMixin       # Part 8
+from django.contrib.auth.decorators import permission_required  # Part 9
+import datetime
+from .forms import RenewBookForm
 # Create your views here.
 
 def index(request):
@@ -106,3 +110,158 @@ class LoanedBooksByUserListView(LoginRequiredMixin, generic.ListView):
 
     def get_queryset(self):
         return BookInstance.objects.filter(borrower=self.request.user).filter(status__exact='o').order_by('due_back')
+
+# Added as part of challenge!
+from django.contrib.auth.mixins import PermissionRequiredMixin
+
+# Added as part of challenge!
+class LoanedBooksAllListView(PermissionRequiredMixin, generic.ListView):
+    """Generic class-based view listing all books on loan. Only visible to users with can_mark_returned permission."""
+    model = BookInstance
+    permission_required = 'catalog.can_mark_returned'
+    template_name = 'catalog/bookinstance_list_borrowed_all.html'
+    paginate_by = 10
+
+    def get_queryset(self):
+        return BookInstance.objects.filter(status__exact='o').order_by('due_back')
+
+
+
+from django.shortcuts import get_object_or_404
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+import datetime
+from django.contrib.auth.decorators import permission_required
+
+# from .forms import RenewBookForm
+from catalog.forms import RenewBookForm
+
+# Part 9
+@permission_required('catalog.can_mark_returned')
+def renew_book_librarian(request, pk):
+    """
+    View function for renewing a specific BookInstance by librarian
+    """
+    book_inst=get_object_or_404(BookInstance, pk = pk)
+
+    # If this is a POST request then process the Form data
+    if request.method == 'POST':
+
+        # Create a form instance and populate it with data from the request (binding):
+        form = RenewBookForm(request.POST)
+
+        # Check if the form is valid:
+        if form.is_valid():
+            # process the data in form.cleaned_data as required (here we just write it to the model due_back field)
+            book_inst.due_back = form.cleaned_data['renewal_date']
+            book_inst.save()
+
+            # redirect to a new URL:
+            return HttpResponseRedirect(reverse('all-borrowed') )
+
+    # If this is a GET (or any other method) create the default form.
+    else:
+        proposed_renewal_date = datetime.date.today() + datetime.timedelta(weeks=3)
+        form = RenewBookForm(initial={'renewal_date': proposed_renewal_date,})
+
+    return render(request, 'catalog/book_renew_librarian.html', {'form': form, 'bookinst':book_inst})
+
+
+
+# renew_book_librarian用於讀書館員幫讀者手動更新書的到期日
+# ****************改用modelform的方式實做！***********
+from django.shortcuts import get_object_or_404
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+import datetime
+
+# 用modelform方式實做的話，與之前的差異點：需改 import RenewBookModelForm
+# from .forms import RenewBookForm
+from .forms import RenewBookModelForm
+
+
+def renew_book_librarian_modelform(request, pk):
+    book_inst = get_object_or_404(BookInstance, pk=pk)
+
+    if request.method == 'POST':
+
+        # 用modelform方式實做的話，與之前的差異點：需改用RenewBookModelForm去做欄位驗證
+        # form = RenewBookForm(request.POST)
+        form = RenewBookModelForm(request.POST)
+
+        # Check if the form is valid:
+        if form.is_valid():
+            # 用modelform方式實做的話，與之前的差異點：欄位名稱需改成due_back
+            # book_inst.due_back = form.cleaned_data['renewal_date']
+            book_inst.due_back = form.cleaned_data['due_back']
+            book_inst.save()
+
+            return HttpResponseRedirect(reverse('all-borrowed'))
+
+    else:
+        proposed_renewal_date = datetime.date.today() + datetime.timedelta(weeks=3)
+
+        # 用modelform方式實做的話，與之前的差異點：欄位名稱需改成due_back
+        # form = RenewBookForm(initial={'renewal_date': proposed_renewal_date,})
+        form = RenewBookModelForm(initial={'due_back': proposed_renewal_date, })
+
+    return render(request, 'catalog/book_renew_librarian_modelform.html', {'form': form, 'bookinst': book_inst})
+
+
+
+#modelform實做範例
+#利用Django的skeleton快速建立create, update, delete功能
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from .models import Author
+
+class AuthorCreate(CreateView):
+    model = Author
+    #選取Author資料表全部的欄位
+    fields = '__all__'
+    initial={'date_of_death':'05/01/2018',}
+
+class AuthorUpdate(UpdateView):
+    model = Author
+    #選取特定欄位
+    fields = ['first_name','last_name','date_of_birth','date_of_death']
+
+class AuthorDelete(DeleteView):
+    model = Author
+    #刪除成功之後，自動導向到下列的網址
+    success_url = reverse_lazy('authors')
+
+
+
+#建立Author資料的List清單網頁
+from django.views import generic
+
+#這是class-based views的限制網頁必須登入的作法
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+class AuthorListView(LoginRequiredMixin, generic.ListView):
+# class AuthorListView(generic.ListView):
+    model = Author
+    #透過定義get_queryset()就可以自己定義想要的資料
+    #沒有要自定義的話就註解掉get_queryset()
+    def get_queryset(self):
+        # return Author.objects.filter(title__icontains='bike')[:5] #取前五筆資料，title包含關鍵字'bike'的
+        return Author.objects.filter()[:100] #取前100筆資料
+    #等等要去哪個路徑找.html檔案
+    #不定義這個template_name的話，Django就會去預設的路徑尋找.html
+    #預設的路徑是：/locallibrary/catalog/templates/catalog/author_list.html
+    #不過目前暫時程式碼設定路徑的方式跟預設一樣就好
+    template_name = '/locallibrary/catalog/templates/catalog/author_list.html'
+
+    #get_context_data()是用來建立自訂的Server side variable的
+    #跟.Net MVC也挺像的
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get the context
+        context = super(AuthorListView, self).get_context_data(**kwargs)
+        # Create any data and add it to the context
+        context['some_data'] = 'This is just some data'
+        return context
+
+    #這是分頁機制, 以下設定每頁最多10筆資料
+    paginate_by = 10
+
